0fde35e17d28bb2fa2698b0a6dc77896bb9a4f24
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-08-09 17:44:56 +0200

    Update 1401-create-user.js

--- src/db/migrations/1401-create-user.js
+++ src/db/migrations/1401-create-user.js
@@ -42,7 +42,7 @@ module.exports = {
                     },
                     key: 'id',
                 },
-                allowNull: false,
+                allowNull: true,
             },
         });
     },
