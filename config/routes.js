bd91e0a96234fb9a7b2703a830c9b9709d8dc54f
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-08-09 17:42:37 +0200

    reroute post/create to signup as its basically the same

--- config/routes.js
+++ config/routes.js
@@ -16,7 +16,7 @@ let routes = {
         actions: [
             { path: '/api/users', action: 'getAll', method: 'GET' },
             { path: '/api/users/:id', action: 'getOne', method: 'GET' },
-            { path: '/api/users', action: 'create', method: 'POST' },
+            { path: '/api/users', action: 'signup', method: 'POST' },
             { path: '/api/users/:id', action: 'update', method: 'PUT' },
             { path: '/api/users/:id', action: 'delete', method: 'DELETE' },
             { path: '/api/signin', action: 'signin', method: 'POST' },
