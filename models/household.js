53c2a7a2d5e17494318bb7d748a36eb21efc6334
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-08-20 15:48:39 +0200

    no need for extendInclude

--- models/household.js
+++ models/household.js
@@ -1,22 +1,4 @@
 module.exports = function (Model, db) {
-    Model.extendInclude = [
-        {
-            model: db.Entry,
-            as: 'entries',
-            attributes: [
-                'id',
-                'name',
-                'income',
-                'timeStamp',
-                'value',
-                'purpose',
-                'interval',
-                'householdId',
-                'categoryId',
-            ],
-        },
-    ];
-
     Model.prototype.writeRemotes = function (data) {
         const self = this;
 
