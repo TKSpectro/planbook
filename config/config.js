7826892bf001d922437b908497daedf2f3d9d977
Tom Käppler <45234980+TKSpectro@users.noreply.github.com>
2020-10-22 15:40:29 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     secret:
         process.env.SECRET ||
         'akopkeopqkmwopkeOPAKEOWPJAIOWjIOANMDIKOAJ123123asmdokn918293SASD',
-    jwtExpiresAfter: 24 * 60 * 60 * 1000,
+    jwtExpiresAfter: process.env.jwtExpiresAfter || 24 * 60 * 60 * 1000,
     cookieName: '_wab_auth_jwt',
-    minify: true,
+    minify: process.env.minify || true,
 };
