0adfdf5e517cd1fc2836384aa66f05cc8283a4bf
Tom Käppler <45234980+TKSpectro@users.noreply.github.com>
2021-03-01 09:56:34 +0100

    add some more seeder data

--- src/db/seeders/1151-demo-users.js
+++ src/db/seeders/1151-demo-users.js
@@ -13,7 +13,7 @@ module.exports = {
                     email: 'tom@mail.com',
                     passwordHash:
                         '$2b$10$nsNf6L5XPv6wOHcC.oWzCuh/dDeFPuEmSYm5IZ4iJyOLe4OAsrIA2', // bCrypt 10 12345678
-                    isAdmin: true,
+                    isAdmin: false,
                 },
                 {
                     id: 2,
@@ -22,6 +22,50 @@ module.exports = {
                     firstName: 'Max',
                     lastName: 'Mustermann',
                     email: 'max@mail.com',
+                    passwordHash:
+                        '$2b$10$nsNf6L5XPv6wOHcC.oWzCuh/dDeFPuEmSYm5IZ4iJyOLe4OAsrIA2', // bCrypt 10 12345678
+                    isAdmin: false,
+                },
+                {
+                    id: 3,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    firstName: 'Bilal',
+                    lastName: 'Alnaani',
+                    email: 'bilal@mail.com',
+                    passwordHash:
+                        '$2b$10$nsNf6L5XPv6wOHcC.oWzCuh/dDeFPuEmSYm5IZ4iJyOLe4OAsrIA2', // bCrypt 10 12345678
+                    isAdmin: false,
+                },
+                {
+                    id: 4,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    firstName: 'Olga',
+                    lastName: 'Klassen',
+                    email: 'olga@mail.com',
+                    passwordHash:
+                        '$2b$10$nsNf6L5XPv6wOHcC.oWzCuh/dDeFPuEmSYm5IZ4iJyOLe4OAsrIA2', // bCrypt 10 12345678
+                    isAdmin: false,
+                },
+                {
+                    id: 5,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    firstName: 'Jens',
+                    lastName: 'Käppler',
+                    email: 'jens@mail.com',
+                    passwordHash:
+                        '$2b$10$nsNf6L5XPv6wOHcC.oWzCuh/dDeFPuEmSYm5IZ4iJyOLe4OAsrIA2', // bCrypt 10 12345678
+                    isAdmin: false,
+                },
+                {
+                    id: 6,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    firstName: 'Bettina',
+                    lastName: 'Käppler',
+                    email: 'bettina@mail.com',
                     passwordHash:
                         '$2b$10$nsNf6L5XPv6wOHcC.oWzCuh/dDeFPuEmSYm5IZ4iJyOLe4OAsrIA2', // bCrypt 10 12345678
                     isAdmin: false,
