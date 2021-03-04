c8739c5199510a99b40d0a4c000411fd914df097
Tom Käppler <45234980+TKSpectro@users.noreply.github.com>
2021-03-04 19:29:45 +0100

    update node-sass and gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
             },
         },
         apidoc: {
-            taskboard: {
+            planbook: {
                 src: 'src/apidoc/',
                 dest: 'docs/',
             },
