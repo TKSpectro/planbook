8c285fab6e2590ec4986dc7301c1a09fc97d2b5a
Tom Käppler <45234980+TKSpectro@users.noreply.github.com>
2021-03-04 11:27:31 +0100

    use env. file in config

--- config/config.js
+++ config/config.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
     secret:
         process.env.SECRET ||
         'akopkeopqkmwopkeOPAKEOWPJAIOWjIOANMDIKOAJ123123asmdokn918293SASD',
-    jwtExpiresAfter: process.env.jwtExpiresAfter || 24 * 60 * 60 * 1000,
+    jwtExpiresAfter:
+        Number(String(process.env.jwtExpiresAfter)) || 24 * 60 * 60 * 1000,
     cookieName: '_wab_auth_jwt',
     minify: process.env.minify || true,
 };
