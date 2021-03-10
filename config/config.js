1063c2a68b8d34f961cf287a452a23f3b0441b4e
Tom Käppler <45234980+TKSpectro@users.noreply.github.com>
2021-03-10 11:00:10 +0100

    add css minify and scss minify

--- config/config.js
+++ config/config.js
@@ -7,5 +7,5 @@ module.exports = {
     jwtExpiresAfter:
         Number(String(process.env.JWT_EXPIRES_AFTER)) || 24 * 60 * 60 * 1000,
     cookieName: 'planbook_auth_jwt',
-    minify: process.env.MINIFY || true,
+    minify: process.env.MINIFY == 'false' ? false : true,
 };
